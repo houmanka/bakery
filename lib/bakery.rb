7d6e041e5ca930f9b44aea1d10e681fcc353c9bf
Houman kargaran <houman.kargaran@gmail.com>
2017-06-21 01:23:05 +1000

    Most of the tests are written now

--- lib/bakery.rb
+++ lib/bakery.rb
@@ -1,34 +1,35 @@
 # require 'shiny_and_mysterious_gem'
 class Bakery
 
-  def initialize
+    def initialize
+        @inventory = [
+            {
+                :item => 'VS5',
+                :packs => [
+                    [ 3, '$6.99' ],
+                    [ 5, '$8.99' ]
+                ]
+            },
+            {
+                :item => 'MB11',
+                :packs => [
+                    [ 2, '$9.95'  ],
+                    [ 5, '$16.95' ],
+                    [ 8, '$24.95' ]
+                ]
+            },
+            {
+                :item => 'CF',
+                :packs => [
+                    [ 3, '$5.95' ],
+                    [ 5, '$9.95' ],
+                    [ 9, '$16.99']
+                ]
+            }
+        ]
+    end
 
-    @inventory = [
-        {
-            :item => 'VS5',
-            :packs => [
-                [ 3, '$6.99' ],
-                [ 5, '$8.99' ]
-            ]
-        },
-        {
-            :item => 'MB11',
-            :packs => [
-                [ 2, '$9.95'  ],
-                [ 5, '$16.95' ],
-                [ 8, '$24.95' ]
-            ]
-        },
-        {
-            :item => 'CF',
-            :packs => [
-                [ 3, '$5.95' ],
-                [ 5, '$9.95' ],
-                [ 9, '$16.99']
-            ]
-        }
-    ]
 
-  end
+
 
 end
