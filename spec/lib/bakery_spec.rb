4699695fc3fd0ca9c0ab778777212c905192e4e6
Houman kargaran <houman.kargaran@gmail.com>
2017-06-21 00:38:32 +1000

    Inventory Pass

--- spec/lib/bakery_spec.rb
+++ spec/lib/bakery_spec.rb
@@ -2,15 +2,19 @@ require 'bakery'
 
 describe Bakery do
 
-  context "Bakery" do
+  context 'Bakery' do
 
-    it "expects to be available" do
+    it 'expects to be available' do
       expect(Bakery.new).to be_instance_of(Bakery)
     end
 
-    it "expects to have an Inventory" do
+      context 'Inventory' do
+        it 'expects to have a Hash of Inventory' do
+          bakery = Bakery.new
+          expect(bakery.instance_variable_get(:@inventory)).to be_instance_of(Array)
+        end
+      end
 
-    end
 
 
   end
